7d4b358274ca9d23c3db3d3d39933d4d674a61f2
Swapneel Bobba <swapneel@mimicry.finance>
2022-08-24 16:11:32 +0530

    add chakra ui

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 
-import "../styles/glo.css";
+import "../styles/globals.css";
+
+// 1. Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+    brand: {
+        900: "#1a365d",
+        800: "#153e75",
+        700: "#2a69ac",
+    },
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
 
 export default MyApp;
