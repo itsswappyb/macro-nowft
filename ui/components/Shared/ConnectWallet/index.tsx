97475e7bd238b327c5d45ddc1bca2453131c5f66
Swapneel Bobba <swapneel@mimicry.finance>
2022-08-28 16:47:05 +0530

    upgrade frontend to use rainbowkit

--- ui/components/Shared/ConnectWallet/index.tsx
+++ ui/components/Shared/ConnectWallet/index.tsx
@@ -1,31 +1,115 @@
-import React from 'react'
-import { ConnectKitButton } from "connectkit";
-import { shortenAddress } from '@utils/shortenAddress'
-import { Menu } from '@headlessui/react'
-import { UserCircleIcon } from '@heroicons/react/outline'
+import React from "react";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { shortenAddress } from "@utils/shortenAddress";
+import { Menu } from "@headlessui/react";
+import { UserCircleIcon } from "@heroicons/react/outline";
 
 const index = () => {
-  return (
-    <div>
-      <ConnectKitButton.Custom>
-        {({ isConnected, isConnecting, show, hide, address, ensName }) => {
-          return (
-            <div>
-              {isConnected ? 
-                (<Menu.Button className="flex bg-transparent text-white py-2 px-6 border border-spritzig rounded-2xl">
-                  <UserCircleIcon className="w-5 h-5 my-auto mr-1"/>
-                  <p>{shortenAddress(address)}</p>
-                </Menu.Button>) : 
-                (<div onClick={show} className="bg-transparent text-white py-2 px-6 border border-spritzig rounded-2xl hover:cursor-pointer hover:bg-spritzig">
-                    Connect Wallet
-                </div>)
-              }
-            </div>
-          );
-        }}
-      </ConnectKitButton.Custom>
-    </div>
-  )
-}
+    return (
+        <ConnectButton.Custom>
+            {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                authenticationStatus,
+                mounted,
+            }) => {
+                // Note: If your app doesn't use authentication, you
+                // can remove all 'authenticationStatus' checks
+                const ready = mounted && authenticationStatus !== "loading";
+                const connected =
+                    ready &&
+                    account &&
+                    chain &&
+                    (!authenticationStatus || authenticationStatus === "authenticated");
 
-export default index
+                return (
+                    <div
+                        {...(!ready && {
+                            "aria-hidden": true,
+                            style: {
+                                opacity: 0,
+                                pointerEvents: "none",
+                                userSelect: "none",
+                            },
+                        })}
+                    >
+                        {(() => {
+                            if (!connected) {
+                                return (
+                                    <button
+                                        onClick={openConnectModal}
+                                        type="button"
+                                        className="bg-transparent
+                                            text-white
+                                            py-2
+                                            px-6
+                                            border
+                                            border-spritzig
+                                            rounded-2xl
+                                            hover:cursor-pointer
+                                            hover:bg-spritzig"
+                                    >
+                                        Connect Wallet
+                                    </button>
+                                );
+                            }
+
+                            if (chain.unsupported) {
+                                return (
+                                    <button onClick={openChainModal} type="button">
+                                        Wrong network
+                                    </button>
+                                );
+                            }
+
+                            return (
+                                <div style={{ display: "flex", gap: 12 }}>
+                                    <button
+                                        onClick={openChainModal}
+                                        style={{ display: "flex", alignItems: "center" }}
+                                        type="button"
+                                    >
+                                        {chain.hasIcon && (
+                                            <div
+                                                style={{
+                                                    background: chain.iconBackground,
+                                                    width: 12,
+                                                    height: 12,
+                                                    borderRadius: 999,
+                                                    overflow: "hidden",
+                                                    marginRight: 4,
+                                                }}
+                                            >
+                                                {chain.iconUrl && (
+                                                    <img
+                                                        alt={chain.name ?? "Chain icon"}
+                                                        src={chain.iconUrl}
+                                                        style={{ width: 12, height: 12 }}
+                                                    />
+                                                )}
+                                            </div>
+                                        )}
+                                        {chain.name}
+                                    </button>
+
+                                    <button onClick={openAccountModal} type="button">
+                                        {account.displayName}
+                                        {account.displayBalance
+                                            ? ` (${account.displayBalance})`
+                                            : ""}
+                                    </button>
+                                </div>
+                            );
+                        })()}
+                    </div>
+                );
+            }}
+        </ConnectButton.Custom>
+    );
+};
+
+export default index;
